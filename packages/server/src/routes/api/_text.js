c85f80fd30bf7953ce8e1da08f6d9a0aea76a7d6
David <davidedri5394@gmail.com>
2020-04-06 19:32:07 +0300

    few fixes

---
+++ packages/server/src/routes/api/_text.js
@@ -0,0 +1,10 @@
+export default {
+  emailExist: "Email address already in use",
+  emailNotFound: "Email address not found",
+  serverError: "Server error try again later",
+  passOrEmailError: "Invalid Email or Password",
+  registerInstructions: (mail) =>
+    `Follow the instructions sent to the mail: ${mail}`,
+  tokenLinkError: "Token Invalid or Expired",
+  passwordChanged: "Password changed successfully",
+};
